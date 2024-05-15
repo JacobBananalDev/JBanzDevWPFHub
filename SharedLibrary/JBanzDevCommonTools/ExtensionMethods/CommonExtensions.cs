9f11fbe817714a0c1709a41ba368f0535cdf1507
Jacob Bananal <jacob.bananal@tek84.com>
2024-05-15 14:38:16 -0700

    JbanzDevCommonTools - Added CommonExtensions.cs

---
+++ SharedLibrary/JBanzDevCommonTools/ExtensionMethods/CommonExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace JBanzDevCommonTools.ExtensionMethods
+{
+    public static class CommonExtensions
+    {
+        public static bool IsNotEmpty(this string value)
+        {
+            return !string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNotNull<T>(this T obj) where T : class
+        {
+            return obj != null;
+        }
+    } 
+}
