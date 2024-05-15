493875612c001113f3775430b1a4e5b4403342f4
Jacob Bananal <jacob.bananal@tek84.com>
2024-05-15 13:48:36 -0700

    JBanzDevUIMatrix - Added MatrixButton

---
+++ JBanzDevUIMatrix/Controls/MatrixButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace JBanzDevUIMatrix.Controls
+{
+    public class MatrixButton : Button
+    {
+        static MatrixButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MatrixButton), new FrameworkPropertyMetadata(typeof(MatrixButton)));
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(MatrixButton), new PropertyMetadata(default(CornerRadius)));
+
+
+    }
+}
